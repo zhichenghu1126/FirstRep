ebf3a8d67269ec07cfe0945df001b6c49918bdf1
huzhicheng <zhicheng.hu3@pactera.com>
2018-08-18 14:25:38 +0800

    test branch

--- src/git_test01/test.java
+++ src/git_test01/test.java
@@ -3,9 +3,12 @@ package git_test01;
 public class test {
 
 	public static void main(String[] args) {
+		int a = 1;
 		// TODO Auto-generated method stub
-		System.out.println("hello");
+		if(a==2)
+		System.out.println("hellossss");
 		System.out.println("github");
+		System.out.println("new branch");
 	}
 
 }
